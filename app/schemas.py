c5ac9f843b3148146c41fcfab6c032f2dcbee140
Osman38 <ylmazosman38@gmail.com>
2022-02-12 21:45:39 +0300

    Main API Work

---
+++ app/schemas.py
@@ -0,0 +1,53 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    # rating: Optional[int] = None
+    # yourm sayırı
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[str] = None

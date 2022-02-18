8a7517c7d23e7c101b4764ee4b1273860ae38a36
Osman38 <ylmazosman38@gmail.com>
2022-02-18 15:56:36 +0300

    Main API

--- app/main.py
+++ app/main.py
@@ -2,19 +2,9 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth
-from pydantic import BaseSettings
+from .config import settings
 
-
-class Settings(BaseSettings):
-    path: int
-    database_username: str = 'postgres'
-    secret_key: str = ''
-
-
-settings = Settings()
-
-print(settings.database_password)
-
+print(settings.database_username)
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
